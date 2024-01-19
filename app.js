a042e7bb60337356b6aa951b83b75b6f8be4363d
“xishuianfeng” <xishuianfeng@gmail.com>
2024-01-19 23:04:00 +0800

    引入证书

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const Koa = require('koa');
+const fs = require('fs');
+const https = require('https');
 // 注意require('koa-router')返回的是函数:
 const router = require('./router/router');
+const sslify = require('koa-sslify').default
 
 const app = new Koa();
 
+app.use(sslify())
 // log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
@@ -13,5 +17,12 @@ app.use(async (ctx, next) => {
 // add router middleware:
 app.use(router.routes());
 
-app.listen(3000);
-console.log('app started at port 3000...');
+// 配置ssl
+const options = {
+    key: fs.readFileSync('./tmdbxifeng.xyz_nginx/tmdbxifeng.xyz.key'),
+    cert: fs.readFileSync('./tmdbxifeng.xyz_nginx/tmdbxifeng.xyz_bundle.pem')
+};
+// 修改启动服务
+https.createServer(options, app.callback()).listen(443, () => {
+    console.log(`服务运行中`);
+});
